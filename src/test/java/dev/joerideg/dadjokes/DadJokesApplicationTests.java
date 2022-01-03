af18969ddfa74212bee305f75988c75caad58f50
Joeri de Gooijer <joerideg@gmail.com>
2022-01-03 14:45:47 +0100

    Initial generation of spring app

---
+++ src/test/java/dev/joerideg/dadjokes/DadJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joerideg.dadjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
